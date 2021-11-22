3ec71dc354afb13018a875f4dea070673518535b
ManVeraM <manuel.vera@alumnos.ucn.cl>
2021-11-22 18:31:06 -0300

    principipio
    nos vamos a echar esta wea

---
+++ taller 2/taller 2/ConexMySQL.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Data;
+using System.IO;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace WindowsFormsApp1{
+
+    class ConexMySQL
+    {
+
+        private MySqlConnection conexion;
+
+        public ConexMySQL()
+        {
+
+            var fileStream = new FileStream("config_file.txt", FileMode.Open, FileAccess.Read);
+
+            using (var streamReader = new StreamReader(fileStream, System.Text.Encoding.UTF8))
+            {
+
+                string line = streamReader.ReadLine();
+
+                this.conexion = new MySqlConnection(line);
+
+            }
+
+        }
+
+        public void open()
+        {
+
+            this.conexion.Open();
+
+        }
+
+        public void close()
+        {
+            this.conexion.Close();
+        }
+
+        public string selectQueryScalar(string query, params string[] values)
+        {
+
+            using (MySqlCommand cmd = new MySqlCommand())
+            {
+
+                cmd.Connection = this.conexion;
+                cmd.CommandText = query;
+
+                for (int pos = 0; pos < values.Length; pos += 2)
+                {
+
+                    cmd.Parameters.AddWithValue(values[pos], values[pos + 1]);
+
+                }
+
+                /**
+                 * No se necesita cerrar ni cláusula using,
+                 * por que una vez que rescata valor, se cierra automáticamente
+                 */
+                return cmd.ExecuteScalar().ToString();
+
+            }
+
+        }
+
+        public DataTable selectQuery(string query, params string[] values)
+        {
+
+            using (MySqlCommand cmd = new MySqlCommand())
+            {
+
+                cmd.Connection = this.conexion;
+                cmd.CommandText = query;
+
+                for (int pos = 0; pos < values.Length; pos += 2)
+                {
+
+                    cmd.Parameters.AddWithValue(values[pos], values[pos + 1]);
+
+                }
+
+                using (MySqlDataReader reader = cmd.ExecuteReader())
+                {
+
+                    DataTable dt = new DataTable();
+                    dt.Load(reader);
+
+                    return dt;
+
+                }
+
+            }
+
+        }
+
+        public int executeNonQuery(string query, params string[] values)
+        {
+
+            MySqlTransaction transaction = null;
+
+            int resultado = 0;
+
+            try
+            {
+
+                transaction = this.conexion.BeginTransaction();
+
+                using (MySqlCommand cmd = new MySqlCommand())
+                {
+
+                    cmd.Connection = this.conexion;
+                    cmd.Transaction = transaction;
+
+                    cmd.CommandText = query;
+
+                    for (int pos = 0; pos < values.Length; pos += 2)
+                    {
+
+                        cmd.Parameters.AddWithValue(values[pos], values[pos + 1]);
+
+                    }
+
+                    resultado = cmd.ExecuteNonQuery();
+
+                    //MessageBox.Show(resultado.ToString());
+
+                }
+
+                transaction.Commit();
+
+                return resultado;
+
+            }
+            catch (Exception ex)
+            {
+
+                if (transaction != null)
+                {
+
+                    transaction.Rollback();
+
+                    MessageBox.Show("Rollback");
+
+                }
+                else
+                {
+
+                    MessageBox.Show(ex.Message);
+
+                }
+
+            }
+
+            //No se ejecutó bien
+            return -1;
+
+        }
+
+    }
+
+}
---
+++ taller 2/taller 2/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsFormsApp1;
+
+namespace taller_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ConexMySQL conex = new ConexMySQL() ;
+            conex.open();
+            string query = "SELECT * FROM categoria";
+            DataTable categorias = conex.selectQuery(query);
+            for (int i =0; i< categorias.Rows.Count; i++)
+            {
+                comboBox1.Items.Add(categorias.Rows[i]["Nombre"]);
+            }
+            conex.close();
+        }
+    }
+}
